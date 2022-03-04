11d4603ebd67d64c03e2a9f698545596d3d4a6ae
Mehul <mehulzr@gmail.com>
2022-03-04 23:49:07 +0530

    Placeholder components added for chat messages and user list

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -4,22 +4,20 @@ import { USER_LOGOUT } from "../actions";
 const Header = (props) => {
   return (
     <nav className="navbar" style={{ height: "10vh" }}>
-      <div className="container">
-        <div className="navbar-menu">
-          <div className="navbar-start">
-            <div className="navbar-brand">
-              <div className="navbar-item">Chatty</div>
-            </div>
+      <div className="navbar-menu">
+        <div className="navbar-start">
+          <div className="navbar-brand">
+            <div className="navbar-item">Chatty</div>
           </div>
-          <div className="navbar-end">
-            <div className="navbar-item">
-              <button
-                onClick={() => props.USER_LOGOUT()}
-                className="button is-primary"
-              >
-                Logout
-              </button>
-            </div>
+        </div>
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <button
+              onClick={() => props.USER_LOGOUT()}
+              className="button is-primary"
+            >
+              Logout
+            </button>
           </div>
         </div>
       </div>
--- client/src/components/welcome.js
+++ client/src/components/welcome.js
@@ -1,10 +1,76 @@
 import React from "react";
+import styles from "./welcome.module.css";
 const Welcome = () => {
   return (
     <div className="hero is-fullheight" style={{ minHeight: "90vh" }}>
-      <div className="hero-body">
-        <div className="container has-text-centered">
-          <h1 className="title">Welcome to the Chatty</h1>
+      <div className={styles.app}>
+        <div className={styles.appLeftPart}>
+          <div className={styles.searchInputContainer}>
+            <input
+              className="input is-rounded"
+              typeof="text"
+              placeholder="Search or Start a new chat"
+            ></input>
+          </div>
+          <ul className={styles.chatList}>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+          </ul>
+        </div>
+        <div className={styles.appRightPart}>
+          <ul className={styles.chatHistory}>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+          </ul>
+          <div className={styles.chatInputContainer}>
+            <input
+              className="input is-rounded"
+              type="text"
+              placeholder="Type a message"
+            ></input>
+            <div className={styles.sendIcon}>
+              <i class="fa-solid fa-message fa-2x"></i>
+            </div>
+          </div>
         </div>
       </div>
     </div>
